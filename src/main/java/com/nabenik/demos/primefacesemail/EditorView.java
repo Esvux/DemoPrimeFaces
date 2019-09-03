9f8f7919535f7dca720643703c0be6eb9f861025
Esvin Armando González Monzón <egonzalez@nabenik.com>
2019-09-03 12:23:48 -0600

    Initial commit.

---
+++ src/main/java/com/nabenik/demos/primefacesemail/EditorView.java
@@ -0,0 +1,31 @@
+package com.nabenik.demos.primefacesemail;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author esvin
+ */
+@ManagedBean
+public class EditorView {
+
+    private String text;
+    private String text2;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText2() {
+        return text2;
+    }
+
+    public void setText2(String text2) {
+        this.text2 = text2;
+    }
+
+}
